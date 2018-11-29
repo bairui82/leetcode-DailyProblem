d21fcaf814ca4410ca2d891b711a77ee2bcc3ddb
Bairui Zhan <bairui82@163.com>
2018-11-29 09:55:38 +0800

    Create best_solution.py

---
+++ 026.RemoveDuplicates fromSorted Array/best_solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l=len(nums)
+        if l<=1:
+            return l
+        index = 0
+        for i, num in enumerate(nums)::
+            if nums[i]!=nums[i+1]:
+                nums[index+1]=nums[i+1]
+                index+=1
+        return index+1
+            
